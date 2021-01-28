87dcf2af38ce0f8856fce476532a4fb11c0cd7f7
Pedro Davi <pedrodaviti@gmail.com>
2021-01-28 19:58:51 -0300

    first release blog

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [`gatsby-theme-blog`, `gatsby-theme-blog-darkmode`],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Pedro Davi`,
+    author: `Pedro Davi`,
+    description: `Distributed IT content`,
+    social: [
+      {
+        name: `Linkedin`,
+        url: `https://www.linkedin.com/in/pdrodavi`,
+      },
+      {
+        name: `Github`,
+        url: `https://git.pedrodavi.com.br`,
+      },
+    ],
+  },
+}
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default function Bio() {
+  return (
+    <>
+      Hello There, I'm <Styled.a href="https://pedrodavi.com.br/">Pedro Davi</Styled.a>
+      {` `}
+      .
+      <br />
+      Application Development Senior at Accenture.
+    </>
+  )
+}
